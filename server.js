dc51c6c42d7a24cba866396f3916c8f4fc1cc978
Caethe <c.raven@agir.at>
2020-02-24 18:59:08 +0100

    24 02 20
    APIs zusammengefügt App & CMS in einem

--- server.js
+++ server.js
@@ -4,6 +4,10 @@
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({host: "localhost", user: "admin2", password: "1234", connectionLimit: 5, database: "mphirschmann"});
 
+const fs = require('fs');
+
+// -------------------------------------------------------------------------------------------------------
+
 // -------------------------------------------------------------------------------------------------------
 
 // Für die Connection auf die DB -- GET ALL
@@ -206,7 +210,6 @@ async function deleteAsyncFunction(table, id){
 
 // -------------------------------------------------------------------------------------------------------
 
-
 // Ausgabe im Browser
 const express = require('express');
 const app = express();
@@ -319,15 +322,6 @@ app.post('/posttext', cors(), (req, res) => {
 });
 
 // Tour & Zw.-Tabelle
-// app.post('/posttour', cors(), (req, res) => {
-//   postAsyncFunction("tour", req.body[0].title, req.body[0].reversible, req.body[0].template_id, req.body[0].guide, req.body[0].date).then(function(val){
-//     postAsyncFunction("tourhstation", val.insertId, req.body[1].station_id, req.body[1].media_id, req.body[1].ordernumber).then(function(val){
-//       res.json(val);
-//     });
-//   });
-// });
-
-// Tour & Zw.-Tabelle
 app.post('/posttour', cors(), (req, res) => {
   postAsyncFunction("tour", req.body.title, req.body.reversible, req.body.template_id, req.body.guide, req.body.date).then(function(val){
     res.json(val);
@@ -383,3 +377,170 @@ app.delete('/delete', cors(), (req, res) =>{
 // ------------------------------------------------------------------------
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+// ----------------------------------------------------------------------------------------
+// Für die App
+// Tutorial: https://stackabuse.com/reading-and-writing-json-files-with-node-js/
+
+// Für die Connection auf die DB -- GET ALL
+async function appAsyncFunction() {
+  let conn;
+  let rows;
+  
+  try {
+    conn = await pool.getConnection();
+    if(arguments[0] == "TOUR"){
+      rows = await conn.query("SELECT * FROM " + arguments[0] + ";");
+    }
+
+    else if (arguments[0] == "TOUReinzeln"){
+      rows = await conn.query("SELECT * FROM TOUR WHERE id = " + arguments[1] + ";");
+    }
+
+    else if (arguments[0] == "TOURinformationen"){
+      rows = await conn.query("SELECT * FROM tour_has_station JOIN media ON media_id = media.id LEFT JOIN text ON media.text_id = text.id LEFT " +
+      "JOIN file ON media.file_id = file.id JOIN station ON tour_has_station.station_id = station.id JOIN area ON station.area_id = area.id " +
+      "WHERE tour_id= " + arguments[1] + " ORDER BY ordernumber");
+    }
+
+    else if (arguments[0] == "AREA"){
+      rows = await conn.query("SELECT DISTINCT area.* FROM tour_has_station " + 
+      "JOIN station ON station.id = station_id " +
+      "JOIN area ON area.id = station.area_id " +
+      "WHERE tour_id=" + arguments[1] + ";");
+    }
+
+    else if (arguments[0] == "STATIONTOUR"){
+      rows = await conn.query("SELECT DISTINCT station.* FROM tour_has_station JOIN station ON station.id = tour_has_station.station_id WHERE tour_id=" + arguments[1] + " AND  area_id=" + arguments[2] + ";");
+    }
+
+    else if (arguments[0] == "MEDIATOUR"){
+      rows = await conn.query("SELECT *, text.id AS textId, file.id AS fileId FROM tour_has_station JOIN media ON tour_has_station.media_id = media.id "+
+          "LEFT JOIN text ON media.text_id=text.id LEFT JOIN file ON media.file_id=file.id " +
+          "WHERE tour_id=" + arguments[1] + " AND tour_has_station.station_id=" + arguments[2] + ";");
+    }
+
+    else{
+      console.log("An error occured, while trying to get all.");
+      rows = null;
+    }
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) conn.release(); //release to pool
+  }
+
+    return new Promise(resolve => {
+      resolve(rows);
+    });
+}
+
+// -------------------------------------------------------------------------------------------------------
+// GET all Touren
+let data;
+
+app.get('/tours', cors(), (req, res) => {
+  appAsyncFunction("TOUR").then(function(val){ // ruft die funktion auf und wenn fertig (then), dann wird die funktion mit den return wert von der funktion gemacht und der wert mit val ausgegeben
+    res.json(val);
+  });
+});
+
+app.get('/tour/:id', cors(), async (req, res) => {
+  let tour = await appAsyncFunction("TOUReinzeln", req.params.id);
+
+  let result = {
+    "id": tour[0].id,
+    "title": tour[0].title,
+    "reversible": tour[0].reversible,
+    "template_id": tour[0].template_id,
+    "guide": tour[0].guide,
+    "date": tour[0].date
+  };
+
+result.areas = [];
+let areas = await appAsyncFunction("AREA", tour[0].id);
+
+  // load areas for tour
+  areas.forEach(area => {
+    let areaResult = {
+        "id": area.id,
+        "title": area.title,
+        "position": area.position,
+        "stations": []
+      };
+      result.areas.push(areaResult);
+  });
+
+  // load stations for area
+  for (let i = 0; i < result.areas.length; i++) {
+    let stations = await appAsyncFunction("STATIONTOUR", tour[0].id, areas[i].id);
+
+    stations.forEach(el => {
+      let station = {
+        "id": el.id,
+        "name": el.name
+      };
+
+      result.areas[i].stations.push(station);
+    });
+
+    // load media for station
+    for (let i = 0; i < result.areas.length; i++) {
+      for (let j = 0; j < result.areas[i].stations.length; j++) {
+        let medias = await appAsyncFunction("MEDIATOUR", tour[0].id, result.areas[i].stations[j].id);
+        result.areas[i].stations[j].medias = [];
+
+        medias.forEach(ele => {
+          let media = null;
+
+          if (ele.text_id != null) {
+            media = {
+              "id": ele.textId,
+              "type": "text",
+              "caption": ele.caption,
+              "text": ele.text
+            };
+          } else if (ele.file_id != null) {
+            media = {
+              "id": ele.fileId,
+              "type": "file",
+              "caption": ele.caption,
+              "url": ele.destinationurl
+            };
+          }
+
+          result.areas[i].stations[j].medias.push(media);
+        });
+      }
+    }
+  }
+
+  await res.json(result);
+  
+  data = JSON.stringify(result, null, 2);
+
+  try{
+    fs.mkdirSync('tours');
+  }
+  catch{ console.log("Tours Repository already exists.");}
+  finally{}
+  fs.mkdirSync('tours/tour_' + result.id);
+  fs.mkdirSync('tours/tour_' + result.id + '/media');
+        
+  fs.writeFile('tours/tour_' + result.id + '/route.json', data, (err)=>{
+    if (err) throw err;
+    console.log("Data written to file.");
+  });
+});
+
+// noch prüfen
+app.get('/download/:filename', function(req, res){
+  const file = `${__dirname}/media/` + req.params.filename;
+  console.log(file);
+  res.download(file); // Set disposition and send it.
+});
+
+  // (() => {
+  //   const deletedPaths = del(['tour/**', '!tour']);
+  //   console.log('Deleted files and directories:\n', deletedPaths.join('\n'));
+  // })();
